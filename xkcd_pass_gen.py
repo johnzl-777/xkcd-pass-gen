e61f0067d50a943a16c1100d112e32e39001c5f0
John Long <johnzl@protonmail.ch>
2018-09-29 23:08:13 -0700

    Added improved string display of password

--- xkcd_pass_gen.py
+++ xkcd_pass_gen.py
@@ -6,11 +6,10 @@ i = 0
 word_file = open("wordlist.txt", mode="r")
 word_list = word_file.read().split("\n")
 
-print(len(word_list))
-
-
 while i < 4:
-	password.append(word_list[int.from_bytes(os.urandom(2), byteorder = "big") % len(word_list)])
+
+	int.from_bytes(os.urandom(2), byteorder = "big") % len(word_list)
+	password.append(word_list[])
 	i += 1
 
-print(password)
+print("Your new password is: %s" % ' '.join(password))
