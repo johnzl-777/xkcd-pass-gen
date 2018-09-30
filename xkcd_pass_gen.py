09ca8e9f96b8db4a77dad866733c63e59050ead8
John Long <johnzl@protonmail.ch>
2018-09-29 23:11:28 -0700

    Restored program to working condition from previous commit

--- xkcd_pass_gen.py
+++ xkcd_pass_gen.py
@@ -8,8 +8,8 @@ word_list = word_file.read().split("\n")
 
 while i < 4:
 
-	int.from_bytes(os.urandom(2), byteorder = "big") % len(word_list)
-	password.append(word_list[])
+	password.append(word_list[int.from_bytes(os.urandom(2), byteorder = "big") % len(word_list)])
 	i += 1
 
 print("Your new password is: %s" % ' '.join(password))
+
