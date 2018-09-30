9057ffa26829dc05224db42aa9e269671b4a31e9
John Long <johnzl@protonmail.ch>
2018-09-29 23:15:02 -0700

    reverted to litte-endian byte order

--- xkcd_pass_gen.py
+++ xkcd_pass_gen.py
@@ -8,7 +8,7 @@ word_list = word_file.read().split("\n")
 
 while i < 4:
 
-	password.append(word_list[int.from_bytes(os.urandom(2), byteorder = "big") % len(word_list)])
+	password.append(word_list[int.from_bytes(os.urandom(2), byteorder = "little") % len(word_list)])
 	i += 1
 
 print("Your new password is: %s" % ' '.join(password))
