9705086e0333872e8e4a79740a16bcb07d0eb140
John Long <johnzl@protonmail.ch>
2018-10-05 17:17:58 -0700

    Removed alpha stage UI and password word repretition detection (to be implemented later)

--- xkcd_pass_gen.py
+++ xkcd_pass_gen.py
@@ -1,14 +1,22 @@
 import os
 
 password = []
-i = 0
 
 word_file = open("wordlist.txt", mode="r")
 word_list = word_file.read().split("\n")
 
-while i < 4:
+while True:
+	try: 
+		password_len_option = int(input("Password Length: "))
+		break
+	except ValueError:
+		print("Please only input positive integers.")
 
-	password.append(word_list[int.from_bytes(os.urandom(2), byteorder = "little") % len(word_list)])
+i = 0
+while i < password_len_option:
+	
+	choice = int.from_bytes(os.urandom(2), byteorder = "little") % len(word_list)
+	password.append(word_list[choice])
 	i += 1
 
 print("Your new password is: %s" % ' '.join(password))
